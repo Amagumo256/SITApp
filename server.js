7660f35e522c4892c82ea113d5616d314ea97534
Amagumo256 <86706792+Amagumo256@users.noreply.github.com>
2021-09-13 04:20:21 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 
 const server = app.listen(3000, async () => {
     console.log(`Node.js is listening to PORT: ${server.address().port}`);
+});
+
+server.get("/", (request, response) => {
+    console.log("requested:", request);
+
+    let data;
+    try {
+        data = fs.readFileSync(request.address);
+    } catch(e) {
+        return response.status(404).send();
+    }
+    response.status(200).send(data);
 });
